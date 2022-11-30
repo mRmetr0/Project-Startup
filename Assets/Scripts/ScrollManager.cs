52cc28e8892ff476d6514ada46af1d3cf0144182
JakobDutch <boriskusters@gmail.com>
2022-11-30 15:57:14 +0100

    some things idk tbh (added 1 scene)

--- Assets/Scripts/ScrollManager.cs
+++ Assets/Scripts/ScrollManager.cs
@@ -6,31 +6,35 @@ using UnityEngine.UIElements;
 
 public class ScrollManager : MonoBehaviour
 {
-    private ScrollView _scrollView;
+    private ScrollView scrollView;
     private bool dragging = false;
-    private float mouseYOnDragStart = 0;
-    
-    void Start()
-    {
-        _scrollView = new ScrollView();
-    _scrollView.RegisterCallback<MouseDownEvent>(OnMouseDown, TrickleDown.TrickleDown);  
-    _scrollView.RegisterCallback<MouseUpEvent>(OnMouseUp, TrickleDown.TrickleDown);  
-    _scrollView.RegisterCallback<MouseMoveEvent>(OnMouseMove, TrickleDown.TrickleDown);    
+    private float mouseYonDragStart = 0;
+
+    public void Start()
+    {      
+        scrollView.RegisterCallback<MouseDownEvent>(OnMouseDown, TrickleDown.TrickleDown);
+        scrollView.RegisterCallback<MouseUpEvent>(OnMouseUp, TrickleDown.TrickleDown);
+        scrollView.RegisterCallback<MouseMoveEvent>(OnMouseMove, TrickleDown.TrickleDown);
     }
 
     private void OnMouseMove(MouseMoveEvent evt)
     {
         if (dragging)
-            _scrollView.scrollOffset = new Vector2(0, mouseYOnDragStart-evt.localMousePosition.y);
+        {
+            scrollView.scrollOffset = new Vector2(0, mouseYonDragStart - evt.localMousePosition.y);
+        }
     }
+
     private void OnMouseUp(MouseUpEvent evt)
     {
         dragging = false;
+
     }
+
     private void OnMouseDown(MouseDownEvent evt)
     {
         dragging = true;
-        mouseYOnDragStart = evt.localMousePosition.y;
+        mouseYonDragStart = evt.localMousePosition.y;
     }
 
 }
