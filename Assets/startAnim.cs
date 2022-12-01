fd516430d5c23888b1aa9a0915cfc4af37569571
JakobDutch <boriskusters@gmail.com>
2022-12-01 16:14:14 +0100

    fade in screen, animation for next screen, layout implemented of menu

---
+++ Assets/startAnim.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class startAnim : MonoBehaviour
+{
+    [SerializeField] Animation animation;
+    [SerializeField] GameObject layer;
+    [SerializeField] Button button;
+    void Start()
+    {
+        button.onClick.AddListener(startAnimation);
+    }
+
+    private void startAnimation()
+    {
+        animation.Play();
+    }
+
+    public void disableLayer()
+    {
+        layer.active = false;
+    }
+}
